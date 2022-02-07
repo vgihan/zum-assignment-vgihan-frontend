887175229d1701f8e1241a3e8e58c66b690b96d9
vgihan <jum654@naver.com>
2022-02-07 18:55:32 +0900

    feat: 게시글 데이터를 api 서버로부터 가져오도록 로직 수정

--- src/components/List.ts
+++ src/components/List.ts
@@ -1,36 +1,29 @@
 import { li, span, ul } from "../core/h";
-import "../../public/css/list.scss";
+import { getPost } from "../api/post";
+import { Post } from "../types/post";
 import { Giact } from "../core/giact";
-import { Post, PostSearch } from "../types/post";
+import "../../public/css/list.scss";
 
 function List() {
-  const [post, setPost] = Giact.useState<PostSearch[]>([
-    { id: 0, title: "test 1", date: new Date(), writer: "vgihan" },
-    { id: 1, title: "test 2", date: new Date(), writer: "vgihn" },
-    { id: 2, title: "test 3", date: new Date(), writer: "vgian" },
-    { id: 3, title: "test 4", date: new Date(), writer: "vihan" },
-    { id: 4, title: "test 5", date: new Date(), writer: "vgia" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-    { id: 5, title: "test 6", date: new Date(), writer: "vgian" },
-  ]);
-  const getPostDate = (date: Date) => {
-    return `${date.getFullYear()}.${date.getMonth()}.${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  const [posts, setPosts] = Giact.useState<Post[]>([]);
+
+  const getData = async () => {
+    const data = await getPost();
+    console.log(data);
+    setPosts(data);
   };
+
+  getData();
 
   return {
     template: () =>
       ul({ class: "container center-box" }, [
-        ...post().map((postElement) => {
+        ...posts().map((postElement: Post) => {
           const { id, title, date, writer } = postElement;
           return li({}, [
             span({ class: "post-id" }, [id]),
             span({ class: "post-title" }, [title]),
-            span({ class: "post-date" }, [getPostDate(date)]),
+            span({ class: "post-date" }, [date]),
             span({ class: "post-writer" }, [writer]),
           ]);
         }),
