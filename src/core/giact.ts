a54b24bffb029a25b84ef7e132ed6de09eec9681
vgihan <jum654@naver.com>
2022-02-08 19:30:39 +0900

    fix: useRouter함수 추가

--- src/core/giact.ts
+++ src/core/giact.ts
@@ -1,16 +1,33 @@
 import { Component, ComponentReturnType } from "../types/component";
+import EventManager from "./eventManager";
 import { VirtualDom } from "./virtualDom";
 
 export const Giact = (() => {
   let _state: any = [];
   let _render: Function;
   let _root: ComponentReturnType;
+  let _page: ComponentReturnType;
   let idx = 0;
 
   const render = (component: Component, $parent: HTMLElement) => {
     if (!_root) _root = component();
-    if (!_render) _render = () => VirtualDom.render(_root.template(), $parent);
+    if (!_render)
+      _render = () => {
+        VirtualDom.render(_root.template(), $parent);
+        EventManager.regist();
+      };
     _render();
+  };
+  const useRouter = (
+    initPage: ComponentReturnType
+  ): [() => ComponentReturnType, (newPage: ComponentReturnType) => void] => {
+    _page = initPage;
+    const getPage = () => _page;
+    const setPage = (newPage: ComponentReturnType) => {
+      _page = newPage;
+      if (_render) _render();
+    };
+    return [getPage, setPage];
   };
   const useState = <T>(initState: T): [() => T, (newState: T) => void] => {
     const i = idx++;
@@ -22,6 +39,10 @@ export const Giact = (() => {
     };
     return [getState, setState];
   };
+  const clear = () => {
+    _state = _state.slice(0, 1);
+    idx = 0;
+  };
 
-  return { useState, render };
+  return { useState, render, clear, useRouter };
 })();
