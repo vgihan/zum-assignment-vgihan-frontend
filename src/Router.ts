e5d45caf27f7fb1c9ab8b1a5a03129a8ce6c1b3f
vgihan <jum654@naver.com>
2022-02-09 23:42:06 +0900

    fix: path param을 사용하기 위해 router 수정

--- src/Router.ts
+++ src/Router.ts
@@ -1,24 +1,38 @@
 import { Giact } from "./core/giact";
 import { div } from "./core/h";
 import Main from "./pages/Main";
+import Modify from "./pages/Modify";
 import Write from "./pages/Write";
 import { Component } from "./types/component";
 
-type Router = {
+type RouterMap = {
   [path: string]: Component;
 };
 
 function Router() {
-  const [curPage, setCurPage] = Giact.useRouter(Main());
-
-  const map: Router = {
+  const getPath = () => {
+    const path = document.location.pathname;
+    const pathElements = path.split("/");
+    console.log(pathElements);
+    return {
+      path: `/${pathElements[1]}`,
+      params: pathElements.slice(2, pathElements.length),
+    };
+  };
+  const map: RouterMap = {
     "/": Main,
     "/write": Write,
+    "/modify": Modify,
   };
+  const initPage = getPath();
+  const [curPage, setCurPage] = Giact.useRouter(
+    map[initPage.path](initPage.params)
+  );
   const onChangeLocation = () => {
     Giact.clear();
-    const path = document.location.pathname;
-    setCurPage(map[path]());
+    const nextPage = getPath();
+    console.log(nextPage);
+    setCurPage(map[nextPage.path](nextPage.params));
   };
 
   window.addEventListener("route", onChangeLocation);
