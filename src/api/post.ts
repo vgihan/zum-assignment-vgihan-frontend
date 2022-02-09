2d42cce5b33dc1d00e70ca17ec14f22ac3e0b414
vgihan <jum654@naver.com>
2022-02-09 23:51:21 +0900

    feat: 캐시를 refresh 하는 api 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,9 +1,14 @@
 import { Post, PostGet, PostWrite } from "../types/post";
 import { createQueryString } from "../utils/createQueryString";
-import { getReq, postReq } from "../utils/customFetch";
+import { getReq, postReq, updateGetReq } from "../utils/customFetch";
 
 export const getPost = async (condition: PostGet = {}) => {
   return await (await getReq(`/post${createQueryString(condition)}`)).json();
+};
+export const getRefreshPost = async (condition: PostGet = {}) => {
+  return await (
+    await updateGetReq(`/post${createQueryString(condition)}`)
+  ).json();
 };
 export const setPost = async (newPost: PostWrite): Promise<Post[]> => {
   return await (
