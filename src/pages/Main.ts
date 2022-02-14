af7b3b1c114cbe7d425ea3e72350c927c45da09a
vgihan <jum654@naver.com>
2022-02-14 19:02:19 +0900

    feat: page 기능 추가를 위한 query string 처리

--- src/pages/Main.ts
+++ src/pages/Main.ts
@@ -1,14 +1,17 @@
-import { div } from "../core/h";
+import { div, a, span } from "../core/h";
 import List from "../components/List";
 import Button from "../components/Button";
 import { Router } from "../core/router";
 import { getPost, getRefreshPost } from "../api/post";
 import { Giact } from "../core/giact";
 import { Post } from "../types/post";
+import { ComponentArgument } from "../types/component";
 import "../../public/css/main.scss";
 
-function Main() {
+function Main({ query }: ComponentArgument) {
   const [posts, setPosts] = Giact.useState<Post[]>([]);
+  const start = (parseInt(query.get("page")) - 1) * 12;
+  const end = parseInt(query.get("page")) * 12;
 
   const handleClickWrite = () => {
     Router.route("/write");
@@ -33,7 +36,22 @@ function Main() {
           className: "refresh-btn",
           onClick: handleClickRefresh,
         }),
-        list({ posts: posts() }),
+        div({ class: "center-box row-box column-info" }, [
+          span({}, ["ID"]),
+          span({}, ["Title"]),
+          span({}, ["Date"]),
+          span({}, ["Writer"]),
+        ]),
+        list({
+          posts: query.get("page")
+            ? posts().slice(start, end)
+            : posts().slice(0, 12),
+        }),
+        div({ class: "center-box row-box" }, [
+          ...Array.from({ length: Math.floor(posts().length / 12) + 1 }).map(
+            (v, i) => a({ href: `/?page=${i + 1}` }, [i + 1])
+          ),
+        ]),
         button({
           name: "글쓰기",
           className: "write-btn",
